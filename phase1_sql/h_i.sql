af9d696ab0720177ad6b6f6b8b5286bfa8ed87b7
CamilBouzidi <camilbouzidi@hotmail.com>
2021-03-07 18:50:28 -0500

    Fixed null rating issue

--- phase1_sql/h_i.sql
+++ phase1_sql/h_i.sql
@@ -6,7 +6,7 @@ CREATE VIEW high_ratings AS
 CREATE VIEW low_ratings AS
 	SELECT DISTINCT a.name
 	FROM actors a, movies m
-	WHERE m.mid = a.mid AND m.rating <4.0;
+	WHERE m.mid = a.mid AND (m.rating <4.0 OR m.rating IS NULL);
 
 SELECT COUNT(*)
 FROM low_ratings;
