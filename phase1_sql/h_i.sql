d73dd29204553861a551f79df3a9b604bff3170d
CamilBouzidi <camilbouzidi@hotmail.com>
2021-03-07 18:44:58 -0500

    added distinct

--- phase1_sql/h_i.sql
+++ phase1_sql/h_i.sql
@@ -1,10 +1,10 @@
 CREATE VIEW high_ratings AS
-	SELECT a.name
+	SELECT DISTINCT a.name
 	FROM actors a, movies m
 	WHERE m.mid = a.mid AND m.rating NOTNULL AND m.rating >=4.0;
 
 CREATE VIEW low_ratings AS
-	SELECT a.name
+	SELECT DISTINCT a.name
 	FROM actors a, movies m
 	WHERE m.mid = a.mid AND m.rating <4.0;
 
