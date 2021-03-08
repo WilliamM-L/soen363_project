a206c5134526e9c8901ccde6471f0f9b4b2290a0
CamilBouzidi <camilbouzidi@hotmail.com>
2021-03-07 21:12:45 -0500

    Implemented requested changes

--- phase1_sql/h_ii.sql
+++ phase1_sql/h_ii.sql
@@ -1,3 +1,6 @@
 SELECT COUNT(a.name) AS num_no_flops
 FROM actors a
-WHERE a.name IN (SELECT name FROM high_ratings EXCEPT SELECT name FROM low_ratings);
+WHERE a.name IN (
+    SELECT name FROM high_ratings 
+    EXCEPT SELECT name FROM low_ratings
+    );
--- phase1_sql/h_iii.sql
+++ phase1_sql/h_iii.sql
@@ -1,7 +1,10 @@
 SELECT a.name, COUNT(m.mid) AS num_movies
 FROM actors a, movies m 
-WHERE a.mid = m.mid AND a.name IN 
-	(SELECT name FROM high_ratings EXCEPT SELECT name FROM low_ratings)
+WHERE a.mid = m.mid AND a.name IN (
+	SELECT name FROM high_ratings 
+	EXCEPT 
+	SELECT name FROM low_ratings
+	)
 GROUP BY (a.name)
 ORDER BY num_movies DESC
 LIMIT 10;
