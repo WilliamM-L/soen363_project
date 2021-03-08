3d94ad06170e410bc64f211cd0db7b2264240185
CamilBouzidi <camilbouzidi@hotmail.com>
2021-03-07 20:30:05 -0500

    All caps fix

--- phase1_sql/h_ii.sql
+++ phase1_sql/h_ii.sql
@@ -1,3 +1,3 @@
-select COUNT(a.name) as num_no_flops
-from actors a
-where a.name IN (SELECT name from high_ratings EXCEPT SELECT name from low_ratings);
+SELECT COUNT(a.name) AS num_no_flops
+FROM actors a
+WHERE a.name IN (SELECT name FROM high_ratings EXCEPT SELECT name FROM low_ratings);
