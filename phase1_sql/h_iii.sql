8260c39c25cb058488b41bfb3997fdbf6db9c2f7
CamilBouzidi <camilbouzidi@hotmail.com>
2021-03-07 20:34:54 -0500

    Update h_iii.sql

--- phase1_sql/h_iii.sql
+++ phase1_sql/h_iii.sql
@@ -1,7 +1,7 @@
-select a.name, COUNT(m.mid) as num_movies
-from actors a, movies m 
-where a.mid = m.mid AND a.name IN 
-	(SELECT name from high_ratings EXCEPT SELECT name from low_ratings)
+SELECT a.name, COUNT(m.mid) AS num_movies
+FROM actors a, movies m 
+WHERE a.mid = m.mid AND a.name IN 
+	(SELECT name FROM high_ratings EXCEPT SELECT name FROM low_ratings)
 GROUP BY (a.name)
-ORDER BY num_movies desc
+ORDER BY num_movies DESC
 LIMIT 10;
