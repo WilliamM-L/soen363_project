87a78d6ae4dba9881dd0500460ce0c753973d978
CamilBouzidi <camilbouzidi@hotmail.com>
2021-03-12 16:51:00 -0500

    removed unnecessary comparison

--- phase1_sql/j_3.sql
+++ phase1_sql/j_3.sql
@@ -4,6 +4,6 @@ FROM all_combinations ac
 WHERE ac.name NOT IN (
     SELECT DISTINCT coa.name
     FROM co_actors coa, actors a
-    WHERE coa.name = a.name AND coa.name = ac.name AND ac.mid = a.mid);
+    WHERE coa.name = a.name AND ac.mid = a.mid);
 
 SELECT COUNT(*) FROM non_existent;
