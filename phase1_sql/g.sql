6b20d2533e966e8b8a8b19a89a2c7a703378204c
Judy Mezaber <judymezaber1699@gmail.com>
2021-03-12 21:33:34 -0500

    Create g.sql

---
+++ phase1_sql/g.sql
@@ -0,0 +1,17 @@
+CREATE VIEW lowmov AS 
+SELECT m.year, m.title, m.rating
+FROM movies m, (SELECT year, min(rating) AS minr
+                FROM movies GROUP BY year) a
+WHERE m.year=a.year AND a.minr=m.rating AND m.year>=2005 AND m.year<=2011
+ORDER BY m.year ASC, m.title ASC;
+
+CREATE VIEW highmov AS 
+SELECT m.year, m.title, m.rating
+FROM movies m, (SELECT year, max(rating) AS maxr
+               FROM movies GROUP BY year) a
+WHERE m.year=a.year AND a.maxr=m.rating AND m.year>=2005 AND m.year<=2011
+ORDER BY m.year ASC, m.title ASC;
+
+SELECT * 
+FROM (SELECT * FROM highmov UNION SELECT * FROM lowmov) AS b_w
+ORDER BY b_w.year ASC, b_w.rating ASC, b_w.title ASC;
