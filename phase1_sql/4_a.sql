08c2930bbb43803ee6a55b264c794201d2ada1b8
CamilBouzidi <camilbouzidi@hotmail.com>
2021-03-14 13:12:52 -0400

    Update 4_a.sql

--- phase1_sql/4_a.sql
+++ phase1_sql/4_a.sql
@@ -1,8 +1,5 @@
-CREATE INDEX ind_act_mid on actors (mid);
 CREATE INDEX ind_act_name on actors (name);
 
-CREATE INDEX ind_gen_mid on genres(mid);
 CREATE INDEX ind_gen_gen on genres(genre);
 
-CREATE INDEX ind_tags_mid on tags(mid);
 CREATE INDEX ind_tags_tid on tags(tid);
