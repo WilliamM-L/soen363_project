b83e5185a98abec2eb6941f9c6a5f8454fc0f950
Judy Mezaber <judymezaber1699@gmail.com>
2021-04-17 19:48:26 -0400

    added 2 others pertinent queries for q8

--- phase2/sql/q8.sql
+++ phase2/sql/q8.sql
@@ -3,4 +3,17 @@
 SELECT l.location_description, c.primary_type
 FROM crime c, crime_location l
 WHERE c.cid = l.cid
-GROUP BY l.location_description, c.primary_type
+GROUP BY l.location_description, c.primary_type;
+
+-- what is the most "popular" crime location?
+
+SELECT l.location_description, count(l) AS most_popular_crime_location
+FROM crime_location l
+GROUP BY l.location_description
+ORDER BY count(l) DESC;
+
+-- How many arson cases are recorded for an abandoned building?
+
+SELECT count(*) AS num_of_cases
+FROM crime c, crime_location l
+WHERE c.cid = l.cid AND c.primary_type = 'ARSON' AND l.location_description LIKE 'ABANDONED%';
