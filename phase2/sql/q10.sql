343bd83258b102180e37364b29d6d61c0f54f6ba
Andrea <andreaelhage@hotmail.com>
2021-04-17 17:14:36 -0400

    Modified q10 and q11

--- phase2/sql/q10.sql
+++ phase2/sql/q10.sql
@@ -1,3 +1,4 @@
+-- Count of drug-related crimes per activity related to the drug
 SELECT count(*) FILTER (
                         WHERE fbi_code ='18'
                             AND c.description LIKE '%POSS%') AS possession,
