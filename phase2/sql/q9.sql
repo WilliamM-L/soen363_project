efaee93088b83987869de71d233b853ea5fb8dce
WilliamM-L <w.morin.laberge@gmail.com>
2021-04-19 00:29:07 -0400

    Improved formatting and added context

--- phase2/sql/q9.sql
+++ phase2/sql/q9.sql
@@ -1,11 +1,12 @@
 -- Rate of crime of top 4 drugs
 SELECT count(*) FILTER (
-                        WHERE c.description LIKE '%CANNABIS%') AS Cannabis,
+                         WHERE c.description LIKE '%CANNABIS%') AS Cannabis,
        count (*) FILTER (
                          WHERE c.description LIKE '%CRACK%') AS Crack,
-             count (*) FILTER (
-                               WHERE c.description LIKE '%HEROIN%') AS Heroin,
-                   count (*) FILTER (
-                                     WHERE c.description LIKE '%COCAINE%') AS Cocaine
+       count (*) FILTER (
+                         WHERE c.description LIKE '%HEROIN%') AS Heroin,
+       count (*) FILTER (
+                         WHERE c.description LIKE '%COCAINE%') AS Cocaine
+       (select count(*) from crime where primary_type='NARCOTICS') AS total_narcotics
 FROM public.crime c
 WHERE fbi_code='18';
