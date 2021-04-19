1efb17df12dec58d15ed5def391c5de06044527f
WilliamM-L <w.morin.laberge@gmail.com>
2021-04-19 15:58:08 -0400

    improved q7

--- phase2/sql/q7.sql
+++ phase2/sql/q7.sql
@@ -1,7 +1,7 @@
--- types of crime per district
-
-SELECT pa.district, c.primary_type
+-- top 50 districts with crime of a certain type
+SELECT pa.district, c.primary_type, count(c.cid) as num_crimes
 FROM crime c, crime_police_area pa
-WHERE c.cid = pa.cid
+WHERE c.cid = pa.cid and district IS NOT NULL
 GROUP BY pa.district, c.primary_type
-limit 500
+order by num_crimes desc
+LIMIT 50;
