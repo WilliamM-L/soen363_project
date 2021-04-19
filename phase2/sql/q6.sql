1f5574faf1d199e092ac58405aa3b0116e55b8ac
Judy Mezaber <judymezaber1699@gmail.com>
2021-04-19 17:17:24 -0400

    added new query as a comment

--- phase2/sql/q6.sql
+++ phase2/sql/q6.sql
@@ -5,3 +5,19 @@ FROM crime_date d, crime c
 WHERE d.cid = c.cid AND (c.primary_type LIKE 'THEFT' OR c.primary_type LIKE 'BATTERY' OR c.primary_type LIKE 'CRIMINAL DAMAGE' OR c.primary_type LIKE 'HOMICIDE')
 GROUP BY (d.year, c.primary_type)
 ORDER BY d.year DESC, num_crimes DESC;
+
+-- -- top 3 types of crime + homocide for every year
+
+-- SELECT DISTINCT d.year, c.primary_type, COUNT(c.cid) as num_crimes
+-- FROM crime_date d, crime c
+-- WHERE d.cid = c.cid AND c.primary_type in
+-- (
+-- 	SELECT cr.primary_type
+-- 	FROM crime cr, crime_date cd
+-- 	where d.year = cd.year
+-- 	Group by (cr.primary_type)
+-- 	order by COUNT(cr.cid) desc
+-- 	limit 3
+-- ) or c.primary_type LIKE 'HOMICIDE'
+-- GROUP BY (d.year, c.primary_type)
+-- ORDER BY d.year DESC, num_crimes DESC;
