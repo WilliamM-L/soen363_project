5192c30f7048f3e08dd13da19ae4057c6b84eb2a
WilliamM-L <w.morin.laberge@gmail.com>
2021-04-19 00:49:36 -0400

    Added context to q5

--- phase2/sql/q5.sql
+++ phase2/sql/q5.sql
@@ -1,11 +1,13 @@
 -- the number of arrest for each type of crime
-
-SELECT c.primary_type, count(*) FILTER (
+SELECT c.primary_type,
+count(*) FILTER (
 	WHERE c.arrest = 'True'
-) AS num_of_arrest, 
+) AS num_of_arrest,
 count (*) FILTER (
 	WHERE c.arrest = 'False'
-) AS num_of_nonarrest, count(c.cid) AS total_num_cases
+) AS num_of_nonarrest,
+count(c.cid) AS total_num_cases,
+100*count(*) FILTER (WHERE c.arrest = 'True')/Cast(count(c.cid) as float) as percentage_arrested
 FROM crime c
 GROUP BY primary_type
-ORDER BY num_of_arrest DESC
+ORDER BY percentage_arrested DESC;
